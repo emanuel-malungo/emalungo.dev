081262085022fff1ae8fb138e762d35a4c8fde69
emanuel-malungo <emanuelmalungo856@gmail.com>
2026-03-27 09:51:44 +0100

    init: setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import "@/assets/styles/globals.css";
+import { Inter, Playfair_Display } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Emalungo Dev",
+  description: "Software Developer Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.variable} ${playfair.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline font-serif">Hello world!</h1>
+      <h4 className="text-lg font-sans text-gray-600">
+        Welcome to my portfolio!
+      </h4>
+    </div>
+  );
+}
